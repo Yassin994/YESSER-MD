5b7195422ba55a407315b69892eae945a2e6785c
Yassin994 <aidenyassin115@gmail.com>
2024-09-10 06:12:57 +0300

    Update reboot.js

--- commandes/reboot.js
+++ commandes/reboot.js
@@ -14,12 +14,12 @@ const{repondre,ms,dev,superUser}=com;
 
   if(!superUser)
   {
-    return repondre("This command is for owner or ғʀᴇᴅɪᴇ ᴛᴇᴄʜ");
+    return repondre("This command is for owner or YESSER TEACH");
   }
 
   const {exec}=require("child_process")
 
-    repondre("LUCKY-MD V5 bot Restarting ⏳");
+    repondre("YESSER-MD  bot Restarting ⏳");
 
   exec("pm2 restart all");
   
