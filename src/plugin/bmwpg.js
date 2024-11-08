2369a9e6b22909d8eb910d26b4f3d3d4d476207e
YESSER TECH <aidenyassin115@gmail.com>
2024-11-08 13:37:04 +0300

    Update bmwpg.js

--- src/plugin/bmwpg.js
+++ src/plugin/bmwpg.js
@@ -13,7 +13,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
 
 
-    const text = `*Pong▰▰▰▰▰▰▱▱▱▱ 70${responseTime.toFixed(2)}0 ms*`;
+    const text = `*Pong▰▰▰🖐️🤠 70${responseTime.toFixed(2)}0 ms*`;
     sock.sendMessage(m.from, { text }, { quoted: m });
   }
 }
