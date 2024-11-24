7ef253c8e1c01daafc6e9ee5b8a0ee58621e7e52
Official123-12 <officialstanlee143@gmail.com>
2024-11-25 01:46:30 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -19,8 +19,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "©yessertech",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "255621995482",
+  OWNER_NAME: process.env.OWNER_NAME || "©STANY_TECH",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "255615490076",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
