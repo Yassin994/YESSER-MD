c7627abf8d16a2f69f5c141a77b0136fef74b49b
YESSER TECH <aidenyassin115@gmail.com>
2024-12-28 00:04:11 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    CAPTION: process.env.CAPTION || "code by yessertech ",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
+    READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
+    MODE: process.env.MODE || "public",
+    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/3kdkf6.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M YESSER-MD WHATSAPP BOT 👍🤠",
+    ANTI_LINK: process.env.ANTI_LINK || "true",
+    ANTI_BAD: process.env.ANTI_BAD || "true",
+    PREFIX: process.env.PREFIX || ".",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+    FAKE_TYPING: process.env.FAKE_TYPING || "false",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+    CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "true",
+    HEART_REACT: process.env.HEART_REACT || "true",
+    OWNER_REACT: process.env.OWNER_REACT || "true",
+    BOT_NAME: process.env.BOT_NAME || "yessertech",
+    STATUS_REPLY: process.env.STATUS_REPLY || "`✅ your status seen 👀`",
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+};
